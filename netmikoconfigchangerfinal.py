2914b06428034edd9db2b091a0a90af99aea248e
Nathan Spears <nlspears@hotmail.com>
2019-09-27 14:28:00 -0400

    update

---
+++ netmikoconfigchangerfinal.py
@@ -0,0 +1,32 @@
+##imports python modules needed to work
+from netmiko import ConnectHandler
+import time, sys, getpass, paramiko
+
+##selects the correct Netmiko class based upon the device_type.
+## I then define a network device dictionary consisting of a device_type, ip, username, and password.
+user = raw_input("Enter your SSH username: ")
+pword = getpass.getpass()
+
+device = {
+    'device_type': 'cisco_ios',
+    #'ip': '192.168.43.10',
+    'username': user,
+    'password': pword,
+    #'secret':'password'
+}
+ipfile=open("iplist.txt") #This file contains a list of switch ip addresses.
+#print ("Please doublecheck your configuration in the config file. Please stop and figure out what you're about to do...")
+#device['username']=input("Enter your SSH username:  ")
+#device['password']=getpass.getpass()
+configfile=open("configfile.txt") #opening the config file with the changes you want to push
+configset=configfile.read() ##reads the config file
+configfile.close() #closes the config file
+
+for line in ipfile:
+    device['ip']=line.strip()
+    print("Connecting to Device " + line)
+    net_connect = ConnectHandler(**device)
+    time.sleep(2)
+    print ("Applying Configuration to Device " + line)
+    output = net_connect.send_config_set(configset)
+    print(output)
